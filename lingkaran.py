320f280a4f150eb3187b0be628817569d1c5fce7
1livesfin <ilishaneolapino@gmail.com>
2024-10-22 08:29:10 +0700

    create aplikasi lingkaran

---
+++ lingkaran.py
@@ -0,0 +1,11 @@
+print("===============================")
+print("        RUMUS LINGKARAN        ")
+print("===============================")
+
+PHI = 3.14
+
+r = float(input('Masukan jari-jari: '))
+
+lk = 2*PHI*r
+
+print ('Luas lingkaran:',lk, ' cm')
