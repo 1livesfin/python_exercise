981a84bace41bb29761af4e09e83577b725d4c47
1livesfin <=>
2024-11-07 17:14:44 +0700

    python_exercise

--- tabung.py
+++ tabung.py
@@ -4,10 +4,8 @@ print("=====================================")
 
 PHI = 3.14
 
-v = float(input('Masukan volume tabung \t:'))
 t = float(input('Masukan tinggi tabung \t:'))
 r = float(input('Masukan ruas \t:'))
-lp = float(input('Masukan luas permukaan tabung \t:'))
 
 volume = PHI*r*r*t
 luas p = 2*PHI*r*(r+t)
